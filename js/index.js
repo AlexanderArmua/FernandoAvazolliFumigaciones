7c113b766d2cf0327bdc31c354646a51ecbc24cd
Alexander Armua Abregu <alexander.armua@conekta.com>
2022-02-06 05:25:45 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 const formulario = document.querySelector("#form");
-const $buttonMailto = document.querySelector("#botonOculto");
 
 formulario.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
 
+  const $buttonMailto = document.createElement("button");
   const form = new FormData(this);
-  // console.log(form.get('name'));
   $buttonMailto.setAttribute("href", `mailto:fvcontroldeplagas@gmail.com?subject=${form.get("name")} - Tel: ${form.get("telefono")} - Email: ${form.get("email")} &body=${form.get("message")}`);
   $buttonMailto.click();
 }
