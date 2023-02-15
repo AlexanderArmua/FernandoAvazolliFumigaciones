fc87a6de17bdde3be98e597cff0f9aa1fe458eaa
mercedesmagnelli <mechamagnelli@gmail.com>
2023-02-14 23:44:24 -0300

    fixed bug in sending mails

--- js/index.js
+++ js/index.js
@@ -5,8 +5,9 @@ formulario.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
 
-  const $buttonMailto = document.createElement("button");
+  const $buttonMailto = document.createElement("a");
   const form = new FormData(this);
   $buttonMailto.setAttribute("href", `mailto:fvcontroldeplagas@gmail.com?subject=${form.get("name")} - Tel: ${form.get("telefono")} - Email: ${form.get("email")} &body=${form.get("message")}`);
   $buttonMailto.click();
+  
 }
